619f7ce987cbf6c87426e57fcc3e6e69349ea085
gaius <gaius@gnu.org>
2020-02-20 00:23:33 +0000

    * Initial creation of repository and commit of initial files.

---
+++ combine.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+
+#
+#  a script to merge two repositories together
+#  (the gm2 repository and the gcc repository).
+#  It builds:
+#     * list of commits for each branch
+#     * a commit will consist of a patch and optionally
+#       file move/new file
+#
+#  The script can be run between two hashes (or across the whole log)
+#  although in the case of gm2 the script is only run over a DAG.
+#  The DAG start/end points are either the start of a branch
+#  or when the directory level of gcc/m2 in the repository changes.
+#
+
+findBranches      = 'git branch -a'
+findBranchStart   = 'git merge-base master'
+findBranchCommits = 'git log master..remotes/origin/gcc_9_2_0_gm2'
+findBranchHistory = 'git log --name-status'
+
+
+debugging = False
+patchDir = "."
+
+import os, sys, getopt, fsalias, glob
+
+
+def usage (code):
+    print ("git-combine [-v][-h] startdir")
+    sys.exit (code)
+
+
+def handleOptions ():
+    global patchDir
+    optlist, l = getopt.getopt (sys.argv[1:], ':vhd:')
+    print ("optlist =", optlist)
+    print ("list =", l)
+    for opt in optlist:
+        print (opt)
+        if opt[0] == '-h':
+            usage (0)
+        if opt[0] == '-v':
+            print ("verbose found")
+        if opt[0] == '-d':
+            patchDir = opt[1]
+
+
+#
+#  printf - keeps C programmers happy :-)
+#
+
+def printf (format, *args):
+    print (str (format) % args, end=' ')
+
+
+#
+#  fatal - displays a message and exits
+#
+
+def fatal (format, *args):
+    print (str (format) % args, end=' ')
+    os.sys.exit (1)
+
+
+def localSystem (s):
+    if debugging:
+        printf (s)
+    os.system (s)
+
+
+def getBranchList () :
+    localSystem (findBranches + ' > /tmp/branch-list')
+    branches = []
+    for lines in open ('/tmp/branch-list', 'r').readlines ():
+        l = lines.lstrip ()
+        words = l.split ()
+        if words[0] == '*':
+            branches += [words[1]]
+        else:
+            branches += [words[0]]
+    return branches
+
+
+def getBranchStart (branch):
+    localSystem (findBranchStart + ' ' + branch + ' > /tmp/branch-start')
+    start = open ('/tmp/branch-start', 'r').readline ()
+    return start
+
+
+def findLog (branch):
+    localSystem (findBranchHistory + ' ' + branch + ' > /tmp/branch-log')
+
+
+def isDigit (ch):
+    return ch in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+def isNumCode (line, code):
+    if (len (line) > len (code)) and (line[:len (code)] == code):
+        line = line[len (code):]
+        while isDigit (line[0]) and len (line) > 1:
+            line = line[1:]
+    return (len (line) > 1) and ((line[0] == " ") or (line[0] == "\t"))
+
+
+def isCode (line, code):
+    codewhite = code + "\t"
+    if (len (line) > len (codewhite)) and (line[:len (codewhite)] == codewhite):
+        return True
+    codewhite = code + " "
+    if (len (line) > len (codewhite)) and (line[:len (codewhite)] == codewhite):
+        return True
+    return False
+
+
+def reversePatches ():
+    files = glob.glob ("%s/*.sh" % (patchDir))
+    files.sort ()
+    n = len (files)
+    for i in range (int (n/2)):
+        j = n-i-1
+        os.system ("mv %s t" % (files[i]))
+        os.system ("mv %s %s" % (files[j], files[i]))
+        os.system ("mv t %s" % (files[j]))
+
+
+def peepCommit (num, lines):
+    if num <= len (lines):
+        for line in lines[num:]:
+            line = line.decode ('ISO-8859-1')
+            line = line.rstrip ()
+            if isCode (line, 'commit'):
+                fsalias.nextCommit (line.split ()[-1])
+                return
+
+
+def createDAGList (logfile):
+    os.system ("rm -rf patch-scripts")
+    fsalias.initPatch ()
+    lines = open (logfile, "rb").readlines ()
+    lines.reverse ()
+    peepCommit (0, lines)
+    for num, line in enumerate (lines):
+        line = line.decode ('ISO-8859-1')
+        line = line.rstrip ()
+        if (len (line) == 0) or (line != line.lstrip ()):
+            fsalias.commitMessage (line)
+        elif isCode (line, 'M'):
+            fsalias.modify (line.split ()[-1])
+        elif isCode (line, 'A'):
+            fsalias.create (line.split ()[-1])
+        elif isCode (line, 'D'):
+            # printf ("delete file: %s\n", line)
+            fsalias.rm (line.split ()[-1])
+        elif isNumCode (line, 'R1') or isNumCode (line, 'R0'):
+            # printf ("mv file: %s\n", line)
+            fsalias.mv (line.split ()[-2], line.split ()[-1])
+        elif isCode (line, 'commit'):
+            fsalias.commit (line.split ()[-1])
+            peepCommit (num+1, lines)
+        elif isCode (line, 'Date:'):
+            line = line.lstrip ()
+            fsalias.date (" ".join (line.split ()[1:]))
+        elif isCode (line, 'Author:'):
+            line = line.lstrip ()
+            fsalias.author (" ".join (line.split ()[1:]))
+        elif isCode (line, "Merge:"):
+            printf ("merge: %s\n", line)
+        else:
+            printf ("unknown: %s\n", line)
+            sys.exit (1)
+    fsalias.finishPatch ()
+    reversePatches ()
+
+
+def main ():
+    handleOptions ()
+    fsalias.patchDir (patchDir)
+    branchList = getBranchList ()
+    for branch in branchList:
+        printf ("branch %s starts at %s\n", branch, getBranchStart (branch))
+    findLog (branchList[0])
+    fsalias.safeDir ("gcc/m2/pre")
+    fsalias.allowedDir ("gcc-versionno", "gcc/m2")
+    fsalias.allowedDir ("gcc-versionno", "gcc/gm2")
+    fsalias.allowedDir ("gcc-versionno", "gcc/testsuite/gm2")
+    fsalias.allowedDir ("gcc-versionno", "gcc/testsuite/lib")
+    fsalias.allowedDir ("gcc-versionno", "libgm2")
+    createDAGList ('/tmp/branch-log')
+
+
+main ()
---
+++ fsalias.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python3
+
+#
+#  a module to implement a filesystem alias
+#  it allows the callee to create files and directories which are aliased to
+#  other locations.
+#
+
+import os, sys
+
+poisonedDirs = []
+altDir = ""
+fs = {}
+allowedDirs = []
+commitAuthor = None
+commitDate = None
+commitPatchNo = 0
+patchOpen = False
+patchDirectory = "."
+output = sys.stdout
+oldStdout = sys.stdout
+lastCommit = None
+commitLog = ""
+commitFile = "/tmp/commitmessage"
+existingDirs = []
+
+
+import codecs
+
+def slashescape(err):
+    """ codecs error handler. err is UnicodeDecode instance. return
+    a tuple with a replacement for the unencodable part of the input
+    and a position where encoding should continue"""
+    thebyte = err.object[err.start:err.end]
+    repl = u'\\x'+hex (ord (thebyte))[2:]
+    return (repl, err.end)
+
+codecs.register_error ('slashescape', slashescape)
+
+
+#
+#  printf - keeps C programmers happy :-)
+#
+
+def printf (format, *args):
+    sys.stdout.write (str (format) % args)
+    sys.stdout.flush ()
+
+
+def oprintf (format, *args):
+    global output
+    output.write (str (format) % args)
+    output.flush ()
+
+
+def safeSystem (format, *args):
+    command = str (format) % args
+    command += " || exit 1\n"
+    if os.system (command) != 0:
+        printf ("error: system (%s) failed\n", command)
+        sys.exit (1)
+
+
+#
+#
+#
+
+def initPatch ():
+    global commitPatchNo, patchOpen, output, commitAuthor, commitDate, commitFileNo
+    commitPatchNo +=1
+    d = patchDirectory + "/patch-scripts"
+    safeSystem ("mkdir -p %s", d)
+    d += "/%06d.sh"
+    d = d % commitPatchNo
+    printf ("[%06d]\n", commitPatchNo)
+    output = open (d, "w")
+    oprintf ("#!/bin/bash\n\n")
+    commitDate = None
+    commitAuthor = None
+    commitFileNo = 1
+
+
+def finishPatch ():
+    global output
+    output.flush ()
+    output.close ()
+
+
+#
+#  allowedDir - a directory in which files and directories can be created.
+#
+
+def allowedDir (topstrip, allowed):
+    global allowedDirs
+    allowedDirs += [[topstrip, allowed]]
+
+
+#
+#  safeDir - the directory under which the aliased files are created.
+#
+
+def safeDir (d):
+    global altDir
+    altDir = d
+
+
+def patchDir (d):
+    global patchDirectory
+    patchDirectory = d
+
+#
+#  findPathName - returns the [filename, path]
+#
+
+def findPathName (name):
+    f = name.split (os.path.sep)[-1]
+    p = ""
+    for c in name.split (os.path.sep)[:-1]:
+        p = os.path.join (p, c)
+    return p, f
+
+
+def copyContents (src, dest, gitdirfile = None):
+    global commitFileNo
+    if lastCommit == None:
+        pass
+    else:
+        filename = patchDirectory + "/patch-scripts/"
+        filename += "%06d-%02d.contents" % (commitPatchNo, commitFileNo)
+        commitFileNo += 1
+        if gitdirfile == None:
+            safeSystem ("git show %s:%s > %s", lastCommit, src, filename)
+        else:
+            safeSystem ("git show %s:%s > %s", lastCommit, gitdirfile, filename)
+        makeDir (extractPath (dest))
+        oprintf ("cp -p %s %s || exit 1\n", filename, dest)
+        output.flush ()
+
+
+def makeDir (path):
+    global existingDirs
+    if True:
+        # if not (path in existingDirs):
+        oprintf ("mkdir -p %s\n", path)
+        oprintf ("git add %s\n", path)
+        return
+        existingDirs += [path]
+        p = "."
+        for c in path.split (os.path.sep):
+            p = os.path.join (p, c)
+            oprintf ("git add %s\n", p)
+
+
+def extractPath (filename):
+    p, f = findPathName (filename)
+    return p
+
+
+def git_sh_create (fullname, path, filename):
+    makeDir (path)
+    copyContents (fullname, fullname)
+    oprintf ("git add %s   # git_sh_create\n", fullname)
+
+
+def temp_git_sh_create (temp, path, filename):
+    combined = os.path.join (path, filename)
+    makeDir (extractPath (temp))
+    copyContents (combined, temp)
+    oprintf ("git add %s  # temp_git_sh_create %s\n", temp, lastCommit)
+
+
+def starts_with (line, word):
+    if (len (line) == len (word)) and (line == word):
+        return True
+    if (len (line) > len (word)) and (line[:len (word)] == word):
+        return True
+    return False
+
+
+def top_strip (d):
+    if (len (d) > 1) and (d[0] == "/"):
+        return d[1:]
+    if (len (d) == 2) and (d == "./"):
+        return ""
+    if (len (d) > 2) and (d[:2] == "./"):
+        return d[2:]
+    return d
+
+
+def isAllowed (d):
+    printf ("directory = %s, allowed: ", d)
+    orig = top_strip (d)
+    for top, allowed in allowedDirs:
+        d = orig
+        if starts_with (d, top):
+            d = d[len (top):]
+            d = top_strip (d)
+            if starts_with (d, allowed):
+                printf ("yes\n")
+                return True
+    printf ("no\n")
+    return False
+
+
+#
+#  create - creates file called name.
+#
+
+def create (fullname):
+    global fs
+    path, filename = findPathName (fullname)
+    # printf ("path = %s, file = %s\n", path, filename)
+    if isAllowed (path):
+        git_sh_create (fullname, path, filename)
+    else:
+        fs[fullname] = os.path.join (os.path.join (altDir, path), filename)
+        temp_git_sh_create (fs[fullname], path, filename)
+
+
+def modify (fullname):
+    path, filename = findPathName (fullname)
+    if fullname in fs:
+        copyContents (fs[fullname], fs[fullname], fullname)
+    else:
+        copyContents (fullname, fullname)
+
+
+#
+#  rm - deletes file called name.
+#
+
+def rm (fullname):
+    global fs
+    if fullname in fs:
+        oprintf ("git rm %s\n", fs[fullname])
+        del fs[fullname]
+    else:
+        oprintf ("git rm %s\n", fullname)
+
+
+#
+#  mv - move file src to dest.
+#
+
+def mv (src, dest):
+    global fs
+    if src in fs:
+        new_src = fs[src]
+        del fs[src]
+        src = new_src
+    path, filename = findPathName (dest)
+    if isAllowed (path):
+        oprintf ("if [ ! -f %s ] ; then exit 1 ; fi\n", src)
+        oprintf ("git mv %s %s || exit 1\n", src, dest)
+    else:
+        fs[dest] = os.path.join (os.path.join (altDir, path), filename)
+        makeDir (os.path.join (altDir, path))
+        oprintf ("if [ ! -f %s ] ; then exit 1 ; fi\n", src)
+        oprintf ("git mv %s %s || exit 2\n", src, fs[dest])
+        path, filename = findPathName (src)
+        # oprintf ("git rm %s\n", path)
+
+
+def author (name):
+    global commitAuthor
+    commitAuthor = name
+    # printf ("Author: %s\n", commitAuthor)
+
+
+def date (name):
+    global commitDate
+    commitDate = name
+    # printf ("Date: %s\n", commitDate)
+
+
+def commitMessage (msg):
+    global commitLog
+    commitLog = msg + "\n" + commitLog
+
+
+def commit (rsa):
+    global lastCommit, commitLog
+
+    lastCommit = rsa
+    filename = patchDirectory + "/patch-scripts/"
+    filename += "%06d.commit-log" % (commitPatchNo)
+    f = open (filename, 'w')
+    f.write (commitLog)
+    f.close ()
+    command = "git commit"
+    if commitDate != None:
+        command += " --date="
+        command += "\""
+        command += commitDate
+        command += "\""
+    if commitAuthor != None:
+        command += " --author="
+        command += "\""
+        command += commitAuthor
+        command += "\""
+    command += " -F "
+    command += filename
+    command += " -a\n"
+    oprintf (command)
+    oprintf ("echo %s\n", commitDate)
+    finishPatch ()
+    initPatch ()
+    commitLog = ""
+
+
+def nextCommit (rsa):
+    global lastCommit
+    lastCommit = rsa
+
+
+# create ("dir/this/that/file.c")
